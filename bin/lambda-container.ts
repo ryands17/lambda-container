d91ab086ebcbefb9cf55fb079eeff3bd37963614
Ryan <dsouza.ryan.11@gmail.com>
2020-12-09 19:06:41 +0530

    initial setup

--- bin/lambda-container.ts
+++ bin/lambda-container.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { LambdaContainerStack } from '../lib/lambda-container-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { LambdaContainerStack } from '../lib/lambda-container-stack'
 
-const app = new cdk.App();
-new LambdaContainerStack(app, 'LambdaContainerStack');
+const app = new cdk.App()
+new LambdaContainerStack(app, 'LambdaContainerStack')
