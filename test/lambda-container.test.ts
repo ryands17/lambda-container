e605d0f84a3938340964eaa0ecfb7ffc4017b695
Ryan <dsouza.ryan.11@gmail.com>
2020-12-25 19:36:13 +0530

    add CI and deps management

--- test/lambda-container.test.ts
+++ test/lambda-container.test.ts
@@ -1,13 +1,24 @@
-// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-// import * as cdk from '@aws-cdk/core';
-// import * as LambdaContainer from '../lib/lambda-container-stack';
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as LambdaContainer from '../lib/lambda-container-stack'
 
-// test('Empty Stack', () => {
-//     const app = new cdk.App();
-//     // WHEN
-//     const stack = new LambdaContainer.LambdaContainerStack(app, 'MyTestStack');
-//     // THEN
-//     expectCDK(stack).to(matchTemplate({
-//       "Resources": {}
-//     }, MatchStyle.EXACT))
-// });
+test('ECR repository and Lambda are created', () => {
+  const app = new cdk.App()
+  const stack = new LambdaContainer.LambdaContainerStack(app, 'LambdaContainer')
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::ECR::Repository', {
+      ImageScanningConfiguration: {
+        scanOnPush: true,
+      },
+    })
+  )
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::Function', {
+      Code: { ImageUri: {} },
+      Role: {},
+      PackageType: 'Image',
+    })
+  )
+})
